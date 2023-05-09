ef4b8185919096e8350cd020bd2f8b334e2ee28d
anand5232 <jhariyaanand000@gmail.com>
2023-05-09 20:33:07 +0530

    java code

---
+++ src/CalculatorByMethod.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class CalculatorByMethod {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calculator Using Methods");
+        System.out.println("Choose One Options: ");
+        System.out.println(" 1. Addition ");
+        System.out.println("2. Substraction ");
+        System.out.println("3. Division ");
+        System.out.println("4. Multiplication ");
+
+        int choice = input.nextInt();
+        System.out.println();
+
+        if (choice == 1) {
+            addition();
+        } else if (choice == 2) {
+            substraction();
+        } else if (choice == 3) {
+            division();
+        } else if (choice == 4) {
+            multiplication();
+        }
+        System.out.println();
+        input.close();
+    }
+
+    public static void addition() {
+        int num1, num2, result;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Addition");
+
+        System.out.println("Enter First Number");
+        num1 = input.nextInt();
+        System.out.println("Enter Second Number");
+        num2 = input.nextInt();
+        result = num1 + num2;
+        input.close();
+        System.out.println(" Sum = " + result);
+    }
+
+    public static void substraction() {
+        int num1, num2, result;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Substraction ");
+        System.out.println("Enter First Number");
+        num1 = input.nextInt();
+        System.out.println("Enter Second Number");
+        num2 = input.nextInt();
+        result = num2 - num1;
+        input.close();
+        System.out.println("Result = " + result);
+    }
+
+    public static void division() {
+        int num1, num2, result;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Division");
+        System.out.println("Enter first Number");
+        num1 = input.nextInt();
+        System.out.println("Enter the second number");
+        num2 = input.nextInt();
+
+        result = num2 / num1;
+        input.close();
+        System.out.println("Result = " + result);
+    }
+
+    public  static void multiplication() {
+        int num1, num2, result;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Multiplication");
+        System.out.println("Enter the First Number");
+        num1 = input.nextInt();
+        System.out.println("Enter the Second Number");
+        num2 = input.nextInt();
+        result = num1 * num2;
+        input.close();
+        System.out.println("Result = " + result);
+    }
+
+
+
+
+
+    }
+
---
+++ src/Calculator_Demo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Calculator_Demo {
+    public static void main(String[] args) {
+
+    }
+}
